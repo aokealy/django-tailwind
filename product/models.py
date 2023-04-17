54778c6b318ed10b96e2cc31124dbef6df87fe51
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-17 10:21:12 +0100

    models.py
    created some fields for products such as name, slug, description and price and create a few products in the admin page.

--- product/models.py
+++ product/models.py
@@ -13,5 +13,17 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name     
 
   
