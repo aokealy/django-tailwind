3435dada627158e501ee8a582814a2982f59e557
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-18 11:22:07 +0100

    core/views.py
    add category in the views

--- core/views.py
+++ core/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
-from product.models import Product
+from product.models import Product, Category
 
 def frontpage(request):
     products = Product.objects.all()[0:8]
     return render(request, 'core/frontpage.html', {'products': products})
 
 def shop(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
 
-    return render(request, 'core/shop.html', {'products': products})
+    context = {
+        'categories': categories,
+        'products': products
+    }
+
+    return render(request, 'core/shop.html', context)
 
