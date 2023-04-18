db2ad9e6dcdbb469c7980e1ae33bd4d6a8ea9aca
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-18 14:22:35 +0100

    product_list_item.html
    create django tags for hard code that we can click on product to bring us to that page

--- product/views.py
+++ product/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
-def product(request):
-    return render(request, 'product/product.html')
+from .models import Product
+
+def product(request, slug):
+    product = Product.objects.get(slug=slug)
+    
+    return render(request, 'product/product.html', {'product':product})
