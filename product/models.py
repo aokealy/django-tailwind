c660c72ad43f84a445715bc4ec439d2a1902549f
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-18 12:32:57 +0100

    shop.html
    display correct prices by creating a model to display price and remove the hard code with django tags

--- product/models.py
+++ product/models.py
@@ -24,6 +24,9 @@ class Product(models.Model):
         ordering = ('-created_at',)
 
     def __str__(self):
-        return self.name     
+        return self.name 
+
+    def get_display_price(self):
+        return self.price / 100    
 
   
