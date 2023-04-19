1bdd4236b2e11d524c5f311919f274a9cfe99605
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-19 10:46:23 +0100

    models.py
    generate thumbnail  and change size to fill if no image exists

--- product/models.py
+++ product/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.files import File
+
+from io import BytesIO
+from PIL import Image
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -29,6 +33,29 @@ class Product(models.Model):
         return self.name 
 
     def get_display_price(self):
-        return self.price / 100    
+        return self.price / 100 
 
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save() 
+
+                return self.thumbnail.url 
+            else:
+                return 'https://via.placeholder.com/240x240x.jpg'
+
+    def make_thumbnail(self, image, size=(300, 300)):
+        img = Image.open(image) 
+        img.covert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
   
