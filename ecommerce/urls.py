2bf4c3c7628392e164e56460b73c618cb5404585
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-19 11:52:59 +0100

    views.py signup.html
    use post method for user to be able to signup when creating an account and be redirected to the home page when signed up

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,13 +4,13 @@ from django.contrib import admin
 from django.urls import path
 
 from cart.views import add_to_cart
-from core.views import frontpage, shop, signup, login
+from core.views import frontpage, shop, signup, login_old
 from product.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
-    path('login/', login, name='login'),
+    path('login/', login_old, name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
