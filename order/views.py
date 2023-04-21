a16a0536b4394e9a3c954e39160ae36731e143bd
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-21 18:50:01 +0100

    order/views.py
    created the function for our project to talk to stripe and get payment

--- order/views.py
+++ order/views.py
@@ -2,6 +2,7 @@ import json
 import stripe
 
 from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from cart.cart import Cart
@@ -33,23 +34,36 @@ def start_order(request):
 
         items.append(obj)
 
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        address = request.POST.get('address')
-        zipcode = request.POST.get('zipcode')
-        place = request.POST.get('place')
-        phone = request.POST.get('phone')
 
-        order = Order.objects.create(user=request.user, first_name=first_name, last_name=last_name, email=email, phone=phone, address=address, zipcode=zipcode, place=place)
+    stripe_api_key = settings.STRIPE_API_KEY_HIDDEN 
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=items,
+        mode='payments',
+        sucess_url = 'http://127.0.0.1:8000/cart/success/',
+        cancel_url = 'http://127.0.0.1:8000/cart/'
+    )
+    payment_intent = session.payment_intent
 
-        for item in cart:
-            product = item['product']
-            quantity = int(item['quantity'])
-            price = product.price * quantity
+    first_name = request.data['first_name']
+    last_name = request.data['last_name']
+    email = request.data['email']
+    address = request.data['address']
+    zipcode = request.data['zipcode']
+    place = request.data['place']
+    phone = request.data['phone']
 
-            item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
+    order = Order.objects.create(user=request.user, first_name=first_name, last_name=last_name, email=email, phone=phone, address=address, zipcode=zipcode, place=place)
+    order.payment_intent = payment_intent
+    order.paid_amount = total_price
+    order.paid = True
+    order.save()
 
-        return redirect('myaccount')
-    return redirect('cart')
+    for item in cart:
+        product = item['product']
+        quantity = int(item['quantity'])
+        price = product.price * quantity
+
+        item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
+
+    return JsonResponse({'session':session, 'order': payment_intent})
