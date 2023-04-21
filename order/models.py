d490754b8a05ee93a4fe145ecb809956a04f7415
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-21 12:27:41 +0100

    models.py
    create model order for when item is ordered and shipped and add the fields in views.py

--- order/models.py
+++ order/models.py
@@ -1,8 +1,17 @@
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from django.db import models
 
+from product.models import Product
+
 class Order(models.Model):
-    user = models.ForeignKey(User, related_name='orders', blank=True, null=True on_delete=models.CASCADE)
+    ORDERED = 'ordered'
+    SHIPPED = 'shipped'
+
+    STATUS_CHOICES = (
+        (ORDERED, 'Ordered'),
+        (SHIPPED, 'Shipped')
+    ) 
+    user = models.ForeignKey(User, related_name='orders', blank=True, null=True, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
@@ -12,3 +21,14 @@ class Order(models.Model):
     phone = models.CharField(max_length=255)
 
     created_at = models.DateTimeField(auto_now=True)
+
+    paid = models.BooleanField(default=False)
+    paid_amount = models.IntegerField(blank=True, null=True)
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=ORDERED)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='items', on_delete=models.CASCADE) 
+    price = models.IntegerField()  
+    qauntity = models.IntegerField(default=1) 
--- order/views.py
+++ order/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def start_order(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        zipcode = request.POST.get('zipcode')
+        place = request.POST.get('place')
+        phone = request.POST.get('phone')
+
+        
