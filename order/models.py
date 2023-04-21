7edd95b129585d95128506318ee80d2310b4fd5c
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-21 11:30:47 +0100

    Update models.py
    create models for orders and use foreignkey to link user with ordrers

--- order/models.py
+++ order/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import User
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', blank=True, null=True on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=255)
+    place = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now=True)
