3e6c000b13901c2cea31af1900a72e5ea137d515
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-24 10:54:08 +0100

    order/admin.py
    admin add filters and created_at and status in admin panel for orders

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+class orderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    inlines = [OrderItemInline]   
+
+admin.site.register(Order, orderAdmin)
+admin.site.register(OrderItem)
