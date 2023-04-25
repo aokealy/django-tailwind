499fde3f2bb4019c49c7d6ba9582254e1fa21a0f
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-25 20:17:41 +0100

    update
    update for deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,6 +94,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
+
 
 
 # Password validation
