efccae9d887698041b0adb56ec46e6b7fae8e0df
aokealy <104302143+aokealy@users.noreply.github.com>
2023-04-25 12:00:55 +0100

    robot.txt
    create a robot.txt file and add it in the urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 
 
@@ -13,4 +14,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('order/', include('order.urls')),
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name='core/robots.txt', content_type='text/plain')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
